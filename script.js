9b27c43ffa32c203b3f8fe08b2a5bda8fcc3ecba
Camila <zanellacamila016@gmail.com>
2024-09-27 12:29:51 -0300

    aula10

---
+++ script.js
@@ -0,0 +1,7 @@
+let nome='Camila'
+let sobrenome='Zanella'
+let idade='17'
+
+console.log("Meu nome é",nome,sobrenome,"e minha idade é",idade,"anos");
+let mensagem=`Meu nome é ${nome} ${sobrenome} e minha idade é ${idade} anos`;
+console.log(mensagem);
